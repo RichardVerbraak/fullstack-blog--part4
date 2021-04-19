b4ed273aef9bde572728f24e4d0e8319df6fcb42
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-04-19 16:14:14 +0200

    Total of likes in a blog array tested and passed

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,8 +1,27 @@
-const { dummy } = require('../utils/list_helper')
+const { dummy, totalLikes } = require('../utils/list_helper')
 
 test('dummy returns one', () => {
 	const blogs = []
 
 	const result = dummy(blogs)
 	expect(result).toBe(1)
+})
+
+test('Total of likes in an array of blogs', () => {
+	const blogs = [
+		{
+			title: 'At the Mountains of Madness',
+			author: 'H.P. Lovecraft',
+			url: 'something.something.com',
+			likes: 4,
+		},
+		{
+			title: 'Dagon',
+			author: 'H.P. Lovecraft',
+			url: 'something.something.com',
+			likes: 6,
+		},
+	]
+
+	expect(totalLikes(blogs)).toBe(10)
 })
