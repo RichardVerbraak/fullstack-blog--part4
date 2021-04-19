2351591956a9583bca7346812431d712074bf7cf
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-04-19 16:04:45 +0200

    Jest working, test has passed

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,1 +1,8 @@
-const listHelper = require('../utils/list_helper')
+const { dummy } = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+	const blogs = []
+
+	const result = dummy(blogs)
+	expect(result).toBe(1)
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,4 @@ const dummy = (blogs) => {
 	return 1
 }
 
-module.exports = dummy
+module.exports = { dummy }
