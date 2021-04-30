c5bb1cf466d550140d774953d4d30571e3370c0d
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-04-30 18:04:05 +0200

    Added authentication (verifying token) before adding a new blog

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blogModel')
 const User = require('../models/userModel')
 
@@ -11,21 +12,38 @@ const getAllBlogs = async (req, res) => {
 	res.send(blogs)
 }
 
+// Could refactor the authentication process into it's own middleware
 const addNewBlog = async (req, res, next) => {
 	try {
 		const { title, author, url, likes } = req.body
 
-		const user = await User.findOne()
+		let token
 
-		const blog = new Blog({ title, author, url, likes, user })
+		if (
+			req.headers.authorization &&
+			req.headers.authorization.startsWith('Bearer')
+		) {
+			try {
+				token = req.headers.authorization.split(' ')[1]
 
-		user.blogs = [...user.blogs, blog]
+				const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
 
-		const savedBlog = await blog.save()
-		await user.save()
+				const user = await User.findById(decoded.id)
 
-		res.status(201)
-		res.json(savedBlog)
+				const blog = new Blog({ title, author, url, likes, user })
+
+				user.blogs = [...user.blogs, blog]
+
+				const savedBlog = await blog.save()
+				await user.save()
+
+				res.status(201)
+				res.json(savedBlog)
+			} catch (error) {
+				res.status(401)
+				res.json({ message: 'Invalid token' })
+			}
+		}
 	} catch (error) {
 		res.status(400)
 		next(error)
